a808a515b40e9c6925e8796233128ae2edbe42a7
SoulWind <Soulyu@163.com>
2021-06-10 23:03:17 +0800

    first debug

---
+++ debug-demo/src/main/java/com/soulyu/App.java
@@ -0,0 +1,15 @@
+package com.soulyu;
+
+import com.soulyu.bean.ABean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
+		ABean service = (ABean)context.getBean("loginService");
+		System.out.println(service.toString());
+	}
+}
---
+++ debug-demo/src/main/java/com/soulyu/bean/ABean.java
@@ -0,0 +1,12 @@
+package com.soulyu.bean;
+
+/**
+ * @author Soul.宇
+ */
+public class ABean {
+
+	@Override
+	public String toString() {
+		return "bean create success";
+	}
+}
