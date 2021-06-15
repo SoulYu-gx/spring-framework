984a35e27cab42d0604bdfadfe7acae04527dfae
soul.宇 <soul.yu@soulyudeMacBook-Pro.local>
2021-06-15 15:30:06 +0800

    update

--- debug-demo/src/main/java/com/soulyu/App.java
+++ debug-demo/src/main/java/com/soulyu/App.java
@@ -4,7 +4,7 @@ import com.soulyu.bean.ABean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App {
+public class  App {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
